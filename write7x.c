e442d887a4f81e962ced42c9714ca923108ab326
Robert Pengelly <robertapengelly@hotmail.com>
2022-12-02 15:18:25 +0000

    Simplify the writing to char array and use values from limits.h

--- write7x.c
+++ write7x.c
@@ -1,21 +1,27 @@
 /******************************************************************************
  * @file            write7x.c
  *****************************************************************************/
+#include    <limits.h>
+
 #include    "stdint.h"
 #include    "write7x.h"
 
 void write721_to_byte_array (unsigned char *dest, uint16_t val) {
 
-    dest[0] = (val & 0xFF);
-    dest[1] = (val >> 8) & 0xFF;
+    int i;
+    
+    for (i = 0; i < 2; ++i) {
+        dest[i] = (val >> (CHAR_BIT * i)) & UCHAR_MAX;
+    }
 
 }
 
 void write741_to_byte_array (unsigned char *dest, uint32_t val) {
 
-    dest[0] = (val & 0xFF);
-    dest[1] = (val >> 8) & 0xFF;
-    dest[2] = (val >> 16) & 0xFF;
-    dest[3] = (val >> 24) & 0xFF;
+    int i;
+    
+    for (i = 0; i < 4; ++i) {
+        dest[i] = (val >> (CHAR_BIT * i)) & UCHAR_MAX;
+    }
 
 }
